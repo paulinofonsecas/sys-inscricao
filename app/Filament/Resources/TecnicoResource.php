2477658d988c54c2a088807e1f53ab42308c1fa3
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-05-04 19:17:06 +0100

    Cria a base do recurso tecnico

---
+++ app/Filament/Resources/TecnicoResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TecnicoResource\Pages;
+use App\Filament\Resources\TecnicoResource\RelationManagers;
+use App\Models\Tecnico;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TecnicoResource extends Resource
+{
+    protected static ?string $model = Tecnico::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTecnicos::route('/'),
+            'create' => Pages\CreateTecnico::route('/create'),
+            'view' => Pages\ViewTecnico::route('/{record}'),
+            'edit' => Pages\EditTecnico::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/TecnicoResource/Pages/EditTecnico.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\TecnicoResource\Pages;
+
+use App\Filament\Resources\TecnicoResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditTecnico extends EditRecord
+{
+    protected static string $resource = TecnicoResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
