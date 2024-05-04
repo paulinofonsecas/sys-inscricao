f0afa5feec3d438b6d4cf7f0153786745461f376
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-05-04 19:13:54 +0100

    KAN-29 cria o painel do tecnico

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\Filament\CandidatoPanelProvider::class,
+        App\Providers\Filament\TecnicoPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
